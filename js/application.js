28e7d5f23347014fd5d22ba6ffa628eab7b65800
Cortni <cortni.fernandez@gmail.com>
2021-02-17 12:35:00 -0500

    Add Item working

--- js/application.js
+++ js/application.js
@@ -29,4 +29,30 @@ $( document ).ready(function() {
     updateCartTotal();
   });
 
+  var addItem = function() {
+    var name = $(".new-item").val();
+    var price = $(".new-item-price").val();
+    $(".item-list").append('<div class="row item-row"> \
+    <div class="item-name col-xs-4"> \ '
+    + name + '\
+    </div> \
+    <div class="item-price col-xs-2"> \ '
+    + "$" + price + '\
+    </div> \
+    <div class="item-qty col-xs-2"> \
+      <input class="quantity" type="number"> \
+    </div> \
+    <div class="item-total col-xs-2"></div> \
+    <div class="item-remove col-xs-2"> \
+      <button class="remove">Remove</button> \
+    </div> \
+  </div>');
+  }
+
+  $(document).on('click', '.add', function() {
+    addItem();
+    $(".new-item").val("");
+    $(".new-item-price").val("");
+  })
+
 });
